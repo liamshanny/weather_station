293aebc89c0f0c5c971cde4b6b0bdc3cf0358bd6
dilly <dylanshuler11@gmail.com>
2020-08-03 21:00:04 -0400

    adding rough queue to handle post failures

--- pi/main.py
+++ pi/main.py
@@ -11,14 +11,16 @@ sampleCount = 0
 
 urllib3.disable_warnings()
 
+headers = {"content-type": "application/x-msgpack"}
+cert = ('../backend/certs/user.crt', '../backend/certs/user_unencrypted.key')
+
 def main():
     #check for 4g connection
     #init code
     sensors = sensorPackage(27, ['a','c'])
     dataQueue = SimpleQueue()
     logQueue = SimpleQueue()
-    headers = {"content-type": "application/x-msgpack"}
-    cert = ('../backend/certs/user.crt', '../backend/certs/user_unencrypted.key')
+    
     while(1):
         for x in range(6):
             data = dataLogging.logData(dataQueue, sensors)
@@ -30,11 +32,30 @@ def main():
             dataQueue.put(data)
             time.sleep(sampleInterval)
         
+        if logQueue.empty() == False:
+            #try to make a single post request and test 
+            data = logQueue.get()
+            r = sendData(data)
+            if not(r.ok):
+                logQueue.put(data) #request failed, store it for later
+            else:
+                #this loop is shit and assumes the server is back for good once it is back, change later or else. Purely to test basic queue implementation if power goes tomorrow
+                data = logQueue.get()
+                while logQueue.empty() == False:
+                    r = sendData(data)    
+
         while dataQueue.empty() == False:
             try:
                 data = dataQueue.get(timeout=.5)
-                r = requests.post('https://weather.shanny.tools/api/v1/full-send', data=data, headers=headers, cert=cert, verify=False)
-                print(r.reason)
+                r = sendData(data)
+                if not(r.ok):
+                    logQueue.put(data) #request failed, store it for later
             except Exception as e:
                 print(e)
+
+def sendData(data):
+    r = requests.post('https://weather.shanny.tools/api/v1/full-send', data=data, headers=headers, cert=cert, verify=False)
+    return r
+
+
 main()
